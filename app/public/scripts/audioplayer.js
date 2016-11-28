d863c7e2b90ad9477f20e96b6019fb4cfc260a90
Administrator <1254870992@qq.com>
2016-11-28 16:52:31 +0800

    2016-11-28 audioplayer

--- app/public/scripts/audioplayer.js
+++ app/public/scripts/audioplayer.js
@@ -7,30 +7,32 @@ var audioPlayer = {
       url +
       '">亲 您的浏览器不支持html5的audio标签</audio>';
     $('.voice').append(s);
-
-    this.playCotrol(); //播放控制函数
-
-    var userAgent = navigator.userAgent.toLowerCase(); //取得浏览器的userAgent字符串
-    if (userAgent.match(/MicroMessenger/i) == "micromessenger") {
-
-    } else {
-      $('#control').addClass('pause').removeClass('play');
+    var control = $('#control');
+    var userAgent = navigator.userAgent.toLowerCase();
+    var isSafari = userAgent.indexOf("safari") > -1 && userAgent.indexOf("chrome") < 1 ;
+    if(isSafari){
+      $(control).addClass('pause');
+    }else {
+      $(control).addClass('play');
     }
-
+    this.playCotrol();
     var audio = document.getElementById('audio');
-    if ($('#control').hasClass('play')) {
-      audio.load(); //ios9
-      audio.play();//开始播放
-    }
-
-
-
-/*    var audio = document.getElementById('audio');
-    $(window).one('touchstart click',function(){
+    if ($(control).hasClass('play')) {
       audio.load(); //ios9
       audio.play();
-    });*/
-
+    }
+    this.clicks();
+    /*
+    *   chrome(模拟器iphone): mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)
+    *                         version/9.0 mobile/13b143 safari/601.1
+    *
+    *   safari:               mozilla/5.0 (iphone; cpu iphone os 10_1_1 like mac os x) applewebkit/602.2.14 (khtml, like gecko)
+    *                         version/10.0 mobile/14b100 safari/602.1
+    *
+    *   chrome:               mozilla/5.0 (windows nt 10.0; wow64) applewebkit/537.36 (khtml, like gecko)
+    *                         chrome/54.0.2840.87 safari/537.36
+    *
+    *  */
   },
   clicks: function () {
     var audio = document.getElementById('audio');
@@ -38,70 +40,36 @@ var audioPlayer = {
       if ($('#control').hasClass('play')) {
         audio.pause();
       } else {
-        alert('click');
-        audio.play();//开始播放
+        audio.play();
       }
     })
   },
   playCotrol: function () {
     var audio = document.getElementById('audio');
-    audio.addEventListener('loadeddata', //歌曲一经完整的加载完毕
+    audio.addEventListener('loadstart', function() {
+      console.log('loadstart');
+    }, false);
+    audio.addEventListener('loadeddata',
       function() {
-        audioPlayer.clicks();
+        console.log('loadeddata');
       }, false);
-    audio.addEventListener('error', //歌曲一经完整的加载完毕
+    audio.addEventListener('loadedmetadata', function() {
+      console.log('loadedmetadata');
+    }, false);
+    audio.addEventListener('canplay', function() {
+      console.log('canplay');
+    }, false);
+    audio.addEventListener('error',
       function() {
+        console.log('error');
       }, false);
     audio.addEventListener('pause',
-      function() { //监听暂停
+      function() {
         $('#control').addClass('pause').removeClass('play');
       }, false);
     audio.addEventListener('play',
-      function() { //监听播放
+      function() {
         $('#control').addClass('play').removeClass('pause');
       }, false);
   }
 };
-
-
-
-
-  /*function loadVoice(url) {
-    var s = '<audio id="audio" src="' +
-      url +
-      '">亲 您的浏览器不支持html5的audio标签</audio>';
-    $('.voice').append(s);
-    playCotrol(); //播放控制函数
-  }
-
-  //点击播放/暂停
-  function clicks() {
-    var audio = document.getElementById("audio");
-    $("#control").click(function() {
-      if ($("#control").hasClass("play")) {
-        $("#control").addClass("pause").removeClass("play");
-        audio.play();//开始播放
-      } else {
-        $("#control").addClass("play").removeClass("pause");
-        audio.pause();
-      }
-    })
-  }
-  //播放事件监听
-  function playCotrol() {
-    var audio = document.getElementById("audio");
-    audio.addEventListener("loadeddata", //歌曲一经完整的加载完毕
-      function() {
-        clicks();
-      }, false);
-    audio.addEventListener("pause",
-      function() { //监听暂停
-        $("#control").addClass("play").removeClass("pause");
-      }, false);
-    audio.addEventListener("play",
-      function() { //监听播放
-        $("#control").addClass("pause").removeClass("play");
-      }, false);
-  }*/
-
-
